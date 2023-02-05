9bdd2807d0225ad25f31f6ca38220de78f0d868b
Mongo1248 <123602754+Mongo1248@users.noreply.github.com>
2023-02-05 14:49:09 -0500

    done for now added Encrypt=False to connection string

--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,21 @@ namespace SimpleSQLConApp
 
         private void btnConnect_Click(object sender, EventArgs e)
         {
-            string ConnString = "Date Source = DESKTOP-TNTQ7TC\\SQLEXPRESS, Initial Catelog = FlashCardDB ";
-            ConnString = ConnString + "Integrated Security, Encrypted = False";
+            string ConnString = "Data Source=DESKTOP-TNTQ7TC\\SQLEXPRESS;Initial Catalog=FlashCardDB;Integrated Security=True;Pooling=False;Encrypt=False";
 
-           // SqlConnection con = new SqlConnection(ConnString);
+                ////"Data Source=DESKTOP - TNTQ7TC\\SQLEXPRESS; Initial Catalog=FlashCardDB; trusted_connection=true; Encrypted=False";
+                //Data Source=DESKTOP-TNTQ7TC\SQLEXPRESS;Initial Catalog=FlashCardDB;Integrated Security=True;Pooling=False
+
+
+            // ConnString = ConnString + "Integrated Security";Encrypt=False";
+            // 
+
+            SqlConnection con = new SqlConnection(ConnString);
+            con.Open();
+
+            textBox1.Text = "Connected";
+
+            //DESKTOP-TNTQ7TC\SQLEXPRESS
 
         }
     }
